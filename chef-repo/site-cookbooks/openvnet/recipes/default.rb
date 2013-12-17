844eb7ee15e8f41ac5b66950d239db0630415133
OTA Hiroshi <otahiros@gmail.com>
2013-12-18 07:04:20 +0900

    adds recipe for repo

--- chef-repo/site-cookbooks/openvnet/recipes/default.rb
+++ chef-repo/site-cookbooks/openvnet/recipes/default.rb
@@ -7,6 +7,13 @@
 # All rights reserved
 #
 
+yum_repository 'epel' do
+  description 'Extra Packages for Enterprise Linux'
+  mirrorlist 'http://mirrors.fedoraproject.org/mirrorlist?repo=epel-6&arch=$basearch'
+  gpgkey 'http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-6'
+  action :add
+end
+
 yum_repository "openvnet" do
   description "openvnet repository"
   url "http://dlc.openvnet.axsh.jp/packages/rhel/6/vnet/current/"
